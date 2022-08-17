3c3ae1881f485a46efce11fae4a6a35e9b1a26d8
haticekarakus <60700212+haticekarakus@users.noreply.github.com>
2022-08-17 17:43:06 +0300

    Enum Update For Read

--- src/reading/simulation/ReaderTest.java
+++ src/reading/simulation/ReaderTest.java
@@ -4,10 +4,11 @@ public class ReaderTest {
 
 	public static void main(String[] args) {
 		Author author=new Author("Victor Hugo");
-		Book book=new Book("Sefiller","Roman",250,0,true,author);
-		Book book2=new Book("Deniz Ýscileri","Roman",100,20,false,author);
-
+		Book book=new Book("Sefiller","Roman",250,0,true,author,BookType.FICTION);
+		Book book2=new Book("Deniz Ýscileri","Roman",100,20,false,author,BookType.COMEDY);
 		
+		book2.getBookType();
+
 		Reader reader=new Reader("Asli",21,'F');
 		
 		reader.read(book);
