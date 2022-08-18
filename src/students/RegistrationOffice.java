766ed1349e00e045f54295c02bc4b59a042856a8
haticekarakus <60700212+haticekarakus@users.noreply.github.com>
2022-08-18 21:33:12 +0300

    Section1-Hw1

---
+++ src/students/RegistrationOffice.java
@@ -0,0 +1,63 @@
+package students;
+
+import java.util.Date;
+import java.util.Random;
+
+public class RegistrationOffice {
+	public RegistrationOffice() {
+		
+	}
+	public void RegisterStudent(Student student) {
+		if(student instanceof PhdStudent) {
+			PhdStudent phd=(PhdStudent) student;
+			System.out.println("Phd student " +phd.getName()+" has registered");
+		}
+		else if(student instanceof MasterStudent) {
+			MasterStudent master=(MasterStudent) student;
+			System.out.println("Master student "+master.getName()+" has registered." );
+		}
+		else if(student instanceof GraduateStudent) {
+			GraduateStudent grd=(GraduateStudent) student;
+			System.out.println("Graduate student "+grd.getName()+" has registered." );
+		}
+		else if(student instanceof UndergraduateStudent) {
+			UndergraduateStudent ugrd=(UndergraduateStudent) student;
+			System.out.println("Undergraduate student "+ugrd.getName()+" has registered." );
+		}
+		else if(student instanceof VocationalStudent) {
+			VocationalStudent voc=(VocationalStudent) student;
+			System.out.println("vocational student "+voc.getName()+" has registered." );
+		}
+		else {
+			System.out.println("Student " + student.getName() + " has registered.");
+		}
+	}
+	
+	@SuppressWarnings("deprecation")
+	public Student getAStudent() {
+		Student student=null;
+		Random r=new Random();
+		int i=r.nextInt(6);
+		switch(i) {
+		case 1:
+			student=new VocationalStudent(112, "Ayhan", 1999,new Date(1966,05,5), "Computer Science");
+			break;
+		case 2:
+			student=new GraduateStudent(245, "Ece", 2000, new Date(1970,12,1), "Engineer", "Kemal Tez");
+			break;
+		case 3:
+			student=new UndergraduateStudent(123, "Semih", 1998, new Date(1980,05,2), "Physics", "Biolog");
+			break;
+		case 4:
+			student= new MasterStudent(2220, "Esin", 2000, new Date(1980,11,3), "Engineer", "Sila Kara");
+			break;
+		case 5:
+			student=new PhdStudent(345, "Derin", 1999, new Date(1975,11,6), "Engineer", "Burcu Cicek",true);
+			break;
+		default:
+			student=new Student(567, "Ege", 1990, new Date(1968,11,6), "Psychology");
+		}
+		
+		return student;
+	}
+}
